723e46a8ac15757a24886707c67747a5ba15a299
karenportillo-ctrl <karen.portillo@correounivalle.edu.co>
2026-02-16 16:18:03 -0500

    Pruebas de Escritorio

--- MetodologiaSolucionProblemas/Sueldo.py
+++ MetodologiaSolucionProblemas/Sueldo.py
@@ -1,6 +1,6 @@
-v1=int(input("Ingrese el valor de la venta 1"))
-v2=int(input("Ingrese el valor de la venta 2"))
-v3=int(input("Ingrese el valor de la venta 3"))
+v1=int(input("Ingrese el valor de la venta 1: "))
+v2=int(input("Ingrese el valor de la venta 2: "))
+v3=int(input("Ingrese el valor de la venta 3: "))
 c=v1*0.1+v2*0.1+v3*0.1
 sF=420000+c
 print("El total de la comisión es: ", c)
